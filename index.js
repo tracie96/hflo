3e6574414131fa6968e2ca445003dab25e9f9915
gospel4me <tracy@gloqal.co>
2022-05-25 21:40:37 +0100

    m

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 // const mongoose = require('mongoose')
-require('dotenv').config();
+require("dotenv").config();
 
 // Express APIs
-const api = require('./routes/pregnancy.routes')
+const api = require("./routes/pregnancy.routes");
 
 // mongoose
 //   .connect(`mongodb+srv://${process.env.SQL_DB_NAME}:${process.env.SQL_PASSWORD}@cluster0.9zbdr.mongodb.net/qc-app?retryWrites=true&w=majority`)
@@ -17,36 +17,36 @@ const api = require('./routes/pregnancy.routes')
 //   })
 
 // Express settings
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false,
-  }),
-)
-app.use(cors())
+  })
+);
+app.use(cors());
 
 // Serve static resources
 // app.use('/public', express.static('public'))
-app.use('/api', api)
+app.use("/api", api);
 
 // Define PORT
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
+  console.log("Connected to port " + port);
+});
 
 // Express error handling
 // app.use((req, res, next) => {
 //     setImmediate(() => {
 //       next(new Error('Something went wrong'))
-//     })  
- 
+//     })
+
 // })
 
 app.use(function (err, req, res, next) {
-  console.error(err.message)
-  if (!err.statusCode) err.statusCode = 500
-  res.status(err.statusCode).send(err.message)
-})
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
